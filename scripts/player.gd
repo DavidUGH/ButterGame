60db7c9779e67fc196f18063448d7a0b52a1826d
DavidUzielGH <daviduziel.gh@gmail.com>
2024-01-13 17:32:21 -0600

    Some organization and placeholder graphics.

---
+++ scripts/player.gd
@@ -0,0 +1,35 @@
+extends CharacterBody2D
+
+var life: int
+var spd: int
+var cadence: int
+var damage: int
+var weapon_kit: int
+var can_walk: bool
+var can_attack: bool
+var is_attacking: bool
+var is_hurting: bool
+var can_be_hurt: bool
+var is_slippery: bool
+var is_power_up_anim: bool
+var is_falling: bool
+
+var _sprite: AnimatedSprite2D
+
+func _ready():
+	pass
+
+func _process(delta):
+	pass
+
+func _move():
+	pass
+
+func _is_falling():
+	pass
+	
+func _die():
+	pass
+	
+func _attack():
+	pass
