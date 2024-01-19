12fb0c64c74d1b664ea1718755b966d852a54acb
DavidUzielGH <daviduziel.gh@gmail.com>
2024-01-19 00:23:16 -0600

    added a bunch of jumping functions, it all sucks, fuck me

--- lvl1.gd
+++ lvl1.gd
@@ -15,3 +15,26 @@ func _process(delta):
 			spawn_enemy(butterboy)
 	else:
 		_enemy_spawn_flag = false
+
+func _on_left_area_2d_area_entered(area):
+	var shape = $LeftArea2D/CollisionShape2D.shape
+	var enemy = area.get_parent()
+	enemy.jump(Vector2(shape.extents.x+30, 0))
+
+
+func _on_top_area_2d_area_entered(area):
+	var shape = $TopArea2D/CollisionShape2D.shape
+	var enemy = area.get_parent()
+	enemy.jump(Vector2(0, (shape.extents.y+30)))
+
+
+func _on_bottom_area_2d_area_entered(area):
+	var shape = $BottomArea2D/CollisionShape2D.shape
+	var enemy = area.get_parent()
+	enemy.jump(Vector2(0, -(shape.extents.y+30)))
+
+
+func _on_right_area_2d_area_entered(area):
+	var shape = $RightArea2D/CollisionShape2D.shape
+	var enemy = area.get_parent()
+	enemy.jump(Vector2(-(shape.extents.x+30), 0))
--- scripts/butterboy.gd
+++ scripts/butterboy.gd
@@ -7,12 +7,13 @@ var friction: float
 var life: int
 var _sprite: AnimatedSprite2D
 var player #El nivel tiene que inicializar al jugador
-enum STATE { hurt, moving }
+enum STATE { hurt, moving, jumping}
 var anim_state
 var _hurtbox: Area2D
 var _is_flashing : bool
 var is_hurt = false
 var damage : int
+var is_jumping : bool
 
 func _ready():
 	speed = 60
@@ -22,10 +23,14 @@ func _ready():
 	_is_flashing = false
 	_sprite = $ButterboySprite
 	_hurtbox = $Hurtbox
+	is_jumping = false
 	anim_state = STATE.moving
 	
 func _physics_process(delta):
-	_follow_player(delta)
+	if !is_jumping:
+		_follow_player(delta)
+	else:
+		velocity = Vector2.ZERO
 	move_and_slide()
 	_handle_animations()
 	
@@ -42,6 +47,8 @@ func _handle_animations():
 			_sprite.play("hurt")
 		STATE.moving:
 			_sprite.play("moving")
+		STATE.jumping:
+			_sprite.play("jumping")
 
 func _die():
 	died.emit(position)
@@ -54,6 +61,31 @@ func _flash_white():
 		await get_tree().create_timer(0.2).timeout
 		_is_flashing = false
 		_sprite.material.set_shader_parameter("flash", false)
+
+func jump(jump_vector):
+	is_jumping = true
+	anim_state = STATE.jumping
+	var character_size = Vector2(0,0)
+	if jump_vector.x > 0:
+		character_size = Vector2(30, 0)
+	if jump_vector.x < 0:
+		character_size = Vector2(-30, 0)
+	if jump_vector.y > 0:
+		character_size = Vector2(0, 30)
+	if jump_vector.y < 0:
+		character_size = Vector2(0, -30)
+	print("ch")
+	print(character_size)
+	print("vector")
+	print(jump_vector)
+	var new_position = position+jump_vector+character_size
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "position", new_position, 1)
+	var sprite_position = _sprite.position
+	var sprite_tween = get_tree().create_tween()
+	sprite_tween.tween_property(_sprite, "position", Vector2(0,sprite_position.y-10), 0.5)
+	sprite_tween.tween_property(_sprite, "position", sprite_position, 0.5)
+	_hurtbox.set_deferred("monitoring", false)
 
 func _get_hurt():
 	_flash_white()
@@ -82,3 +114,7 @@ func _on_butterboy_sprite_animation_finished():
 	if anim_state == STATE.hurt:
 		anim_state = STATE.moving
 		is_hurt = false
+	if anim_state == STATE.jumping:
+		anim_state = STATE.moving
+		is_jumping = false
+		_hurtbox.set_deferred("monitoring", true)
