44d7bee9c1350c319f3041a995d2620e0201d66f
Leobardo <avilesleo1106@gmail.com>
2024-01-19 22:22:43 -0600

    NEW TILE SYSTEM

--- scripts/lvl0.gd
+++ scripts/lvl0.gd
@@ -5,6 +5,11 @@ var butterboy = preload("res://butterboy.tscn")
 var GUI
 
 func _ready():
+	for i in range(filas):
+		var fila: Array = []
+		for j in range(columnas):
+			fila.append(0)
+		butter_matrix.append(fila)
 	player = $Player
 	tile_map = $TileMap
 	GUI = $GUI
