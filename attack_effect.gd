6b8217cf231acd36e967a515917a35bee08da39b
DavidUzielGH <daviduziel.gh@gmail.com>
2024-01-20 19:26:10 -0600

    test

--- attack_effect.gd
+++ attack_effect.gd
@@ -1,6 +1,7 @@
 extends AnimatedSprite2D
 
 func _enter_tree():
+	play("default")
 	$AnimationPlayer.play("active_frames")
 
 func _on_animation_finished():
--- scripts/player.gd
+++ scripts/player.gd
@@ -20,9 +20,9 @@ var is_slippery: bool
 var is_power_up_anim: bool
 var is_falling: bool
 var invis_frames = 2
-var weapon_swing
-var kick_swing
-var _is_kicking: bool = false
+var weapon_swing = preload("res://attack_effect.tscn")
+var kick_swing = preload("res://kick_effect.tscn")
+var _is_kicking: bool
 
 var _weapon_sprite: Sprite2D
 var _collision_box: CollisionShape2D
@@ -30,7 +30,6 @@ var _sprite: AnimatedSprite2D
 var _player_hitbox: Area2D
 var _player_hitbox_shape: CollisionShape2D
 var _animation_player: AnimationPlayer
-
 
 func _ready():
 	#Instantiating child nodes
@@ -40,17 +39,18 @@ func _ready():
 	_player_hitbox_shape = $PlayerHitbox/PlayerHitboxShape
 	_weapon_sprite = $KnifeSprite
 	_animation_player = $AnimationPlayer
-	is_hurting = false
+	
 	#Attribute initialization
 	speed = 200
 	life = 100
 	attack_speed = 0.2
 	damage = 1
 	weapon_kit = WEAPON.knife
+	is_hurting = false
 	
 	#Loading scenes to instance
-	weapon_swing = load("res://attack_effect.tscn")
-	kick_swing = load("res://kick_effect.tscn")
+	#weapon_swing = load("res://attack_effect.tscn")
+	#kick_swing = load("res://kick_effect.tscn")
 
 func _process(delta):
 	_move()
@@ -58,7 +58,7 @@ func _process(delta):
 	move_and_slide()
 	_follow_mouse_with_weapon()
 	_attack()
-	_kick()
+	#_kick()
 
 func get_hurt(damage):
 	if !is_hurting:
@@ -114,23 +114,20 @@ func _follow_mouse_with_weapon():
 func _attack():
 	if Input.is_action_just_pressed("attack") and !is_attacking:
 		var weapon_pos = _weapon_sprite.position
-		var weapon_rot = _weapon_sprite.rotation
 		var weapon_swing_spawn = weapon_swing.instantiate()
 		_weapon_sprite.add_child(weapon_swing_spawn)
 		weapon_swing_spawn.position = Vector2(weapon_pos.x+20, 0)
-		weapon_swing_spawn.play("default")
 		is_attacking = true
 		await get_tree().create_timer(attack_speed).timeout
 		is_attacking = false
 
+
 func _kick():
 	if Input.is_action_just_pressed("secondary_attack") and !_is_kicking:
 		var weapon_pos = _weapon_sprite.position
-		var weapon_rot = _weapon_sprite.rotation
 		var kick_swing_spawn = kick_swing.instantiate()
 		_weapon_sprite.add_child(kick_swing_spawn)
 		kick_swing_spawn.position = Vector2(weapon_pos.x+20, 0)
-		kick_swing_spawn.play("default")
 		_is_kicking = true
 		await get_tree().create_timer(1).timeout
 		_is_kicking = false
