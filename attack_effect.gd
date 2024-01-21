9f1eaed05523eeb0b6e621c71c00fc1c880e1aca
DavidUzielGH <daviduziel.gh@gmail.com>
2024-01-20 19:38:43 -0600

    Revert "ye"
    This reverts commit 685a53373583aa245b4af939151014616e661828, reversing
    changes made to b7f32324b025afa1455815d7b5521f1075cded10.

--- attack_effect.gd
+++ attack_effect.gd
@@ -1,7 +1,6 @@
 extends AnimatedSprite2D
 
 func _enter_tree():
-	play("default")
 	$AnimationPlayer.play("active_frames")
 
 func _on_animation_finished():
--- kick_effect.gd
+++ kick_effect.gd
@@ -1,7 +1,4 @@
 extends AnimatedSprite2D
 
-func _enter_tree():
-	play("default")
-
 func _on_animation_finished():
 	queue_free()
