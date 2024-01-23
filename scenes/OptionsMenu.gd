612b218456adb965de645ca870d5ca3498aa4a98
Leobardo <avilesleo1106@gmail.com>
2024-01-22 23:21:19 -0600

    PAUSE MENU

---
+++ scenes/OptionsMenu.gd
@@ -0,0 +1,31 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_music_volume_value_changed(value):
+	pass # Replace with function body.
+
+
+func _on_sf_x_volume_value_changed(value):
+	pass # Replace with function body.
+
+
+func _on_continue_button_pressed():
+	self.position.y = -220
+
+
+func _on_restart_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_exit_button_pressed():
+	pass # Replace with function body.
--- scripts/GUI.gd
+++ scripts/GUI.gd
@@ -3,14 +3,21 @@ extends Control
 
 var life_label : Label
 var console_label: Label
+var pause_menu:Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	life_label = $LifeLabel
 	console_label = $ConsoleLabel
+	pause_menu = $OptionsMenu
 
 func set_life(life):
 	life_label.text = "Life: " + str(life)+"%"
 
 func setConsole(msg):
 	console_label.text = msg
+
+
+func _on_texture_button_pressed():
+	get_tree().paused = true
+	pause_menu.position.y = 50
