c640a4ee8b92b5d8b587ceb6949fa29fb602433e
Leobardo <avilesleo1106@gmail.com>
2024-01-25 22:49:23 -0600

    First Menu

--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -2,7 +2,16 @@ extends Control
 
 @export var event : EventAsset
 
+var fondo : TextureRect
+var atlas : AtlasTexture
+
+func _process(delta):
+	fondo = $TextureRect
+	atlas = fondo.texture
+	atlas.draw_rect_region(atlas.get_rid(), Rect2(0,0,320,240), Rect2(0,0,320,240))
+
 func _on_play_button_pressed():
+	
 	get_tree().change_scene_to_file("res://levels/lvl0.tscn")
 	FMODRuntime.play_one_shot(event)
 
